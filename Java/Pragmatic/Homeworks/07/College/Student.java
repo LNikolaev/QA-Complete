71a71fb25c99b842fe04bccf23d32750b2741910
LNikolaev <luben.lubenov@gmail.com>
2018-07-28 19:39:42 +0300

    remove some propertyes

--- Java/Pragmatic/Homeworks/07/College/Student.java
+++ Java/Pragmatic/Homeworks/07/College/Student.java
@@ -7,19 +7,7 @@ public class Student {
 	public Student(String name, double grade) {
 		this.name = name;
 		this.grade = grade;
-	}
-	
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public double getGrade() {
-		return grade;
-	}
+	}	
 
 	public void setGrade(double grade) {
 		this.grade = grade;
